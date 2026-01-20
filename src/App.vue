51af00e95b367beda67896cd65f9e665cd9cc5f1
Mekan <mekan.developer@gmail.com>
2026-01-20 18:57:27 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="min-h-screen">
+    <header class="flex flex-row justify-between items-center w-full h-[76px] box-border p-4 bg-[var(--bg-primary)] text-[var(--text-primary)] transition-colors ease-in-out duration-300">
+      <HeaderMain />
+    </header>
+
+    <main class="bg-[var(--bg-secondary)] text-[var(--text-secondary)] transition-colors ease-in-out duration-300 p-4 h-[calc(100vh-76px)]">
+      <TheWelcome />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import HeaderMain from './components/headers/HeaderMain.vue'
+</script>
+
+<style>
+/* #app {
+  min-height: 100vh;
+  background: var(--bg-primary);
+  color: var(--text-primary);
+  transition: background-color 0.3s ease;
+} */
+
+
+</style>
---
+++ src/components/headers/ProjectLogo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="w-[160px] h-auto">
+      <img :src="projectLogo" alt="logo elyeterli dal" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import logo_for_dark_mode from '@/assets/logo/for_dark_mode.png'
+import logo_light_mode2 from '@/assets/logo/for_light_mode2.png'
+import { computed, watch } from 'vue'
+import { useTheme } from '@/composables/useTheme'
+const { isDark } = useTheme()
+
+const projectLogo = computed(() => {
+
+  console.log('isDark in ProjectLogo:', isDark.value)
+  return isDark.value ? logo_for_dark_mode : logo_light_mode2
+})
+
+watch(
+  () => isDark.value,
+  (newVal) => {
+    console.log('Theme changed in ProjectLogo. isDark:', newVal)
+  },
+)
+</script>
